55fb1313b6d6926a83e5a9c30e1ea6a817cccdde
honam <75104291+honambsn@users.noreply.github.com>
2024-04-09 23:05:54 +0700

    create new repo, recreate n-tier architecture, category model, subcategory model, item model, appilcation user model, cart model, coupon model, orderheader model, orderdetails model, application dbcontext , data seeding for user, roles, connection string with registration, create admin & customer area

---
+++ FastFood.Repository/DbInitializer.cs
@@ -0,0 +1,60 @@
+﻿using FastFood.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace FastFood.Repository
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public DbInitializer(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                if (_context.Database.GetPendingMigrations().Any())
+                {
+                    _context.Database.Migrate();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred while migrating the database.", ex);
+            }
+
+            if (!_context.Roles.Any(x => x.Name == "Admin"))
+            {
+                _roleManager.CreateAsync(new IdentityRole("Manager")).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole("Admin")).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole("Customer")).GetAwaiter().GetResult();
+            }
+
+            var user = new ApplicationUser()
+            {
+                UserName = "admin@gmail.com",
+                Email = "admin@gmail.com",
+                Name = "Admin",
+                City = "Abc",
+                Address = "xyz",
+                PostalCode = "12345"
+            };
+
+            var result = _userManager.CreateAsync(user, "Admin@123").GetAwaiter().GetResult();
+            if (result.Succeeded)
+            {
+                _userManager.AddToRoleAsync(user, "Admin").GetAwaiter().GetResult();
+            }
+        }
+    }
+}
